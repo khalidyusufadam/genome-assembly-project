bb9b73f26e2550c56aaaf0d640728ea4c578bed7
Khalid Adam Yusuf <106832019+khalidyusufadam@users.noreply.github.com>
2025-04-15 18:04:59 +0100

    upload project files

---
+++ genomeAssemblyApp/src/generalClass.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author khali
+ */
+public class generalClass extends genomeAssemblyFrame {
+     
+       
+}
